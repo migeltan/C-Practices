8ab248f804e5f8a95e7ad1f7130f1dc8dfa38528
Migel Tan <migellltan@gmail.com>
2025-09-01 00:52:30 +0800

    Add files via upload

---
+++ Case Study #1 (Fibonacci Series).cpp
@@ -0,0 +1,36 @@
+/* Group #20
+Members:
+Agojo, Aleckz V.
+Felipe, Cyrus Kishinn
+Tan, Migel H. 
+*/
+
+#include <stdio.h>
+
+int FIBO(int n);
+
+int main() {
+    int months = 12;
+    int RABBITS[months];
+
+    for (int i = 0; i < months; i++) {
+        RABBITS[i] = FIBO(i + 1);
+    }
+
+    printf("Rabbits per month:\n");
+    for (int i = 0; i < months; i++) {
+        printf("Month %d: %d pairs of rabbits\n", i + 1, RABBITS[i]);
+    }
+
+    int total = RABBITS[months - 1];
+
+    printf("\nThe total number of \e[1mrabbits at the end of the year\e[0m is:\e[1m %d pairs \e[0m\n", total);
+
+    return 0;
+}
+
+int FIBO(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;	
+    return FIBO(n - 1) + FIBO(n - 2);
+}
