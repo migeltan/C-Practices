dd7f6ebea6e7ea9d83f8f2f221da994f6efa8706
Migel Tan <migellltan@gmail.com>
2025-09-09 00:11:06 +0800

    Add files via upload

---
+++ Do while.cpp
@@ -0,0 +1,12 @@
+/* Do while loop - also executes a set of statements repeatedly until it
+remains true.
+- condition is checked at the end
+- executes the block of code first, then checks the condition. 
+
+do
+{
+statements;
+}
+while (condition); */
+
+#include<stdio.h>
